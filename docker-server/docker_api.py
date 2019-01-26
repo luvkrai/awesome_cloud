26e66ee23b763284dba9b56190561be96bc4a361
Luv Rai <luvkrai@gmail.com>
2019-01-27 02:18:41 +0530

    corrected docker_server.py

--- docker-server/docker_api.py
+++ docker-server/docker_api.py
@@ -3,27 +3,27 @@ from flask import Flask, jsonify, request
 import json
 
 def initialize_docker():
-    client = docker.DockerClient(base_url='tcp://10.249.112.106:2375', tls=False)
-    return client
+	client = docker.DockerClient(base_url='tcp://10.249.112.106:2375', tls=False)
+	return client
 
 def getDocker():
-    client = initialize_docker()
-    result = []
-    for container in client.containers.list():
-        row = {
-        'name': container.name,
-        'container_id': container.short_id,
-        'image': container.image.tags[0],
-        'status': container.status
-        }
-        result.append(row)
-    return result
+	client = initialize_docker()
+	result = []
+	for container in client.containers.list():
+		row = {
+		'name': container.name,
+		'container_id': container.short_id,
+		'image': container.image.tags[0],
+		'status': container.status
+		}
+		result.append(row)
+	return result
 
 app = Flask(__name__)
 
 @app.route("/docker")
 def docker_container():
-    dummy=[
+	dummy=[
   {
     "container_id": "71686da8da", 
     "image": "alpine:3.8", 
@@ -38,43 +38,17 @@ def docker_container():
   }
 ]
     #return jsonify(getDocker())
-    #return jsonify(dummy)
-    return json.dumps(getDocker())
+	#return jsonify(dummy)
+	return json.dumps(getDocker())
 @app.route("/launch_container",methods=['GET', 'POST'])
 def launch_container():
-    content = request.json
-    client = initialize_docker()
-    container = client.containers.run(content['image'], content['command'],detach=True)
-    if container:
-        return jsonify({'result': 'True'})
-    else:
-        return jsonify({'result': 'False'})
-    
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=50001,debug=True)import docker
-from flask import Flask, jsonify
-import json
-
-def getDocker():
-    client = docker.DockerClient(base_url='tcp://10.249.112.106:2375', tls=False                                                                             )
-    container = client.containers.run('alpine', 'sleep 1000',detach=True)
-    container.logs()
-    result = []
-    for container in client.containers.list():
-    	row = {
-    	'name': container.name,
-    	'container_id': container.short_id,
-    	'image': container.image.tags[0],
-    	'status': container.status
-    	}
-    	result.append(row)
-    return result
-
-app = Flask(__name__)
-
-@app.route("/docker")
-def hello():
-    return jsonify(getDocker())
-    #return json.dumps(getDocker())
+	content = request.json
+	client = initialize_docker()
+	container = client.containers.run(content['image'], content['command'],detach=True)
+	if container:
+		return jsonify({'result': 'True'})
+	else:
+		return jsonify({'result': 'False'})
+	
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=50001,debug=True)
